68794fb5d7019d9e4ae269daf0ead092459ce8f2
ConstLaf <62292727+ConstLaf@users.noreply.github.com>
2020-05-03 20:23:20 -0400

    agrega primeros dos tests

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,114 @@
-import { example, anotherExample } from '../src/data.js';
+import { sortedPokemons, filterPokemons } from '../src/data.js';
 
+const unsortedPokemons = [
+  {
+    "id": 3,
+    "num": "003",
+    "name": "Venusaur",
+    "img": "http://www.serebii.net/pokemongo/pokemon/003.png",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "height": "2.01 m",
+    "weight": "100.0 kg",
+    "candy": "Bulbasaur Candy",
+    "egg": "Not in Eggs",
+  },
+  {
+    "id": 1,
+    "num": "001",
+    "name": "Bulbasaur",
+    "img": "http://www.serebii.net/pokemongo/pokemon/001.png",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "egg": "2 km",
+  },
+  {
+    "id": 2,
+    "num": "002",
+    "name": "Ivysaur",
+    "img": "http://www.serebii.net/pokemongo/pokemon/002.png",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "height": "0.99 m",
+    "weight": "13.0 kg",
+    "candy": "Bulbasaur Candy",
+    "candy_count": 100,
+    "egg": "Not in Eggs",
+  }
+];
 
-describe('example', () => {
+const alphabeticalPokemons = [
+  {
+    "id": 1,
+    "num": "001",
+    "name": "Bulbasaur",
+    "img": "http://www.serebii.net/pokemongo/pokemon/001.png",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "egg": "2 km",
+  },
+  {
+    "id": 2,
+    "num": "002",
+    "name": "Ivysaur",
+    "img": "http://www.serebii.net/pokemongo/pokemon/002.png",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "height": "0.99 m",
+    "weight": "13.0 kg",
+    "candy": "Bulbasaur Candy",
+    "candy_count": 100,
+    "egg": "Not in Eggs",
+  }, {
+    "id": 3,
+    "num": "003",
+    "name": "Venusaur",
+    "img": "http://www.serebii.net/pokemongo/pokemon/003.png",
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    "height": "2.01 m",
+    "weight": "100.0 kg",
+    "candy": "Bulbasaur Candy",
+    "egg": "Not in Eggs",
+  }
+];
+
+describe('sortedPokemons', () => {
   test('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof sortedPokemons).toBe('function');
   });
 
-  test('returns `example`', () => {
-    expect(example()).toBe('example');
+  test('Returns sorted pokemons names from A to Z ', () => {
+    expect(sortedPokemons(unsortedPokemons, 'name', 'az')).toStrictEqual(alphabeticalPokemons);
+  });
+
+  test('Returns sorted pokemons names from Z to A', () => {
+    expect(sortedPokemons(unsortedPokemons, 'name', 'za')).toStrictEqual(alphabeticalPokemons.reverse());
   });
 });
 
-
-describe('anotherExample', () => {
+describe('filterPokemons', () => {
   test('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof filterPokemons).toBe('function');
   });
 
-  test('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  test('Returns pokemons by egg ', () => {
+    expect(filterPokemons(unsortedPokemons, 'egg', '2 km').length).toBe(1);
+  });
+
+  test('Returns pokemons by type ', () => {
+    expect(filterPokemons(unsortedPokemons, 'type', 'Grass').length).toBe(3);
   });
 });
